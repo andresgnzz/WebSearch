1b16fd8ce54087e497e73885032b965b3964335d
andresgnzz <andresgz2002@hotmail.com>
2024-05-20 10:23:07 -0600

    Hice cambios

---
+++ WebSearch-15-04-2024/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace WebSearch_15_04_2024
+{
+    public class WebRequestGetExample
+    {
+        public static void Main()
+        {
+            string artista = "ariana+grande";
+
+            string url = "https://www.setlist.fm/search?query=" + artista;
+
+            // Create a request for the URL. 		
+            WebRequest request = WebRequest.Create(url);
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            // Display the status.
+            Console.WriteLine(response.StatusDescription);
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+            // Cleanup the streams and the response.
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
